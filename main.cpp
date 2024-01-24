fce293e7012e1600ed8b99b404c9d608b3f73e2f
a-necoec1 <72991418+a-necoec1@users.noreply.github.com>
2024-01-24 00:08:44 +0000

    Added in a failsafe to prevent the player from being punished by using a repeat letter, and added in random selection of words instead of just one codeword

--- main.cpp
+++ main.cpp
@@ -1,23 +1,49 @@
 #include "hangman_functions.h"
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <random> // Include the <random> header
 #include <vector>
 
 using namespace std;
 
+string chooseRandomWord(const string &filename) {
+  vector<string> words;
+  ifstream file(filename);
+
+  if (file.is_open()) {
+    string word;
+    while (file >> word) {
+      words.push_back(word);
+    }
+    file.close();
+  } else {
+    cerr << "Unable to open file: " << filename << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // Use random_device to seed the random number generator
+  random_device rd;
+  mt19937 gen(rd());
+
+  // Choose a random word from the vector
+  uniform_int_distribution<> dis(0, words.size() - 1);
+  return words[dis(gen)];
+}
+
 int main() {
   greet();
 
-  // change this so the game randomly takes words from a file instead of a set
-  // code word
-  string codeword = "aaronwashere";
-  string answer = "____________";
+  // Specify the filename containing words (relative path)
+  string filename = "wordfile.txt";
+  string codeword = chooseRandomWord(filename);
+  string answer(codeword.length(), '_');
 
   int misses = 0;
   vector<char> incorrectGuess;
-  bool guess = false;
   char letter;
 
-  // keep the game running until the player wins or loses
+  // Keep the game running until the player wins or loses
   while (answer != codeword && misses < 7) {
     display_misses(misses);
     display_status(incorrectGuess, answer);
@@ -25,24 +51,27 @@ int main() {
     cout << "\n\nPlease enter your guess: ";
     cin >> letter;
 
-    // checks if the guess made is correct or not
+    bool guess = false;
+
+    // Checks if the guess made is correct or not
     for (int i = 0; i < codeword.length(); i++) {
       if (letter == codeword[i]) {
         answer[i] = letter;
         guess = true;
       }
     }
-    // add an else if statement to see if the player has already guessed a
-    // letter already and don't penalize them for it
+
     if (guess) {
       cout << "\nCorrect!\n";
+    } else if (std::find(incorrectGuess.begin(), incorrectGuess.end(),
+                         letter) != incorrectGuess.end()) {
+      cout << "\nYou've already guessed the letter '" << letter
+           << "'. Try again.\n";
     } else {
-      std::cout
-          << "\nIncorrect! Another portion of Mr.Hangman has been drawn.\n";
+      cout << "\nIncorrect! Another portion of Mr.Hangman has been drawn.\n";
       incorrectGuess.push_back(letter);
       misses++;
     }
-    guess = false;
   }
 
   end_game(answer, codeword);
